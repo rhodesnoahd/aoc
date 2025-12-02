9b3dfe3a1df1ad14af7526cb67623a322da36659
Noah Rhodes <rhodesnoahd@gmail.com>
2025-12-01 19:15:07 -0600

    oopsie :)

--- 2025/12-1/sol1.py
+++ 2025/12-1/sol1.py
@@ -13,8 +13,7 @@ for line in lines:
     amount = int(line[1:].strip())
 
     if line[0] == "L":
-        # dial = (dial + (100 - amount)) % 100
-        dial = (100 - amount + dial) % 100
+        dial = (dial + (100 - amount)) % 100
 
     elif line[0] == "R":
         dial = (dial + amount) % 100
